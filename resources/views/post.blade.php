8737be44705c200613f7ab3cc8d8b7dec31e66c0
Marko Romšak <marko.romsak@spletna-postaja.com>
2022-03-30 22:25:35 +0200

    Episode 7

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="post">My First Post</a></h1>
+        <p>
+            Praesentium, tempora, sollicitudin cubilia occaecati nascetur sodales amet eget, sint? Proident, repudiandae placeat interdum? Labore eros. Repellendus ratione nostrum nostrum fusce dictum! Facilisi quia? Aliquam illo curae lacus nisl quos pede accusamus quaerat cursus atque cillum, consequat vulputate vel purus, venenatis laboriosam odio sed, lorem nemo urna bibendum at quibusdam accumsan feugiat est, cum imperdiet! Eum do quasi tortor molestias illo quas massa adipisci iaculis sociosqu pharetra mus doloribus commodi, ornare feugiat, cum eligendi tempus eiusmod senectus duis! Nisl officia congue excepturi morbi consequat platea, montes adipiscing atque, ut irure, incididunt incidunt habitant, porta occaecat, vero conubia sed, provident fusce.
+        </p>
+    </article>
+    <a href="/">Go Back</a>
+</body>
